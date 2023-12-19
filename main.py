86b22b0615dccf02818f7308ddfca940590664db
Alekzum <nbah.dykin@gmail.com>
2023-12-19 22:21:43 +0400

    Бот-пустышка + загрузка из конфига / его создание

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher
+import asyncio
+import logging
 import dotenv
+import json
 
 
-_dot_env_file = ".env" and input("Введите название файла с переменными (примеры: .env или env/.env):")
+# Подсказочки для любимого PyCharm
+_config: dict[str: str]
+_dot_env_file: str
+try:
+    with open("config.json", encoding="utf-8") as file:
+        _config = json.load(file)
+    _dot_env_file = _config["env_file"]
+except FileNotFoundError:
+    _dot_env_file = ".env" and input("Введите название файла с переменными (примеры: .env или env/.env):")
+    _config = {"env_file": _dot_env_file}
+    with open("config.json", encoding="utf-8") as file:
+        json.dump(_config, file, ensure_ascii=False)
+
 _env_in_dirs = dotenv.load_dotenv(_dot_env_file, verbose=True)
 if not _env_in_dirs:
     DICT_API = input("Input your Yandex dictionary API "
@@ -28,3 +46,20 @@ else:
 чел вводит слово, и появляется кнопка *продолжить*, или текст *это слово уже было! предложи другое*
 надо ещё команду /start прикрутить, что бы было, с чего начинать
 """
+
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(BOT_API)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
